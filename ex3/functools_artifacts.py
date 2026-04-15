9996fe9bc77c8a5488e8ca2d1572a3b0d5e39166
miwasaki <4324018@ed.tus.ac.jp>
2026-04-15 11:22:02 +0900

    wrote skeleton of ex3

---
+++ ex3/functools_artifacts.py
@@ -0,0 +1,18 @@
+from functools import reduce, partial, wraps
+import operator
+
+
+def spell_reducer(spells: list[int], operation: str) -> int:
+    pass
+
+
+def partial_enchanter(base_enchantment: Callable) -> dict[str, Callable]:
+    pass
+
+
+def memoized_fibonacci(n: int) -> int:
+    pass
+
+
+def spell_dispatcher() -> Callable:
+    pass
